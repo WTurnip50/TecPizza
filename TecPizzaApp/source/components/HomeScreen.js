1d6f7abc9428275a3f03f786ea54905cad91a71a
Israel Garcia Sanchez <isreal2341@outlook.com>
2026-03-01 00:01:22 -0600

    Initial Commit
    Se creo el proyecto y se creo el esqueleto del mismo sobre el cual se crearan los componentes definidos

---
+++ TecPizzaApp/source/components/HomeScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { SafeAreaView, Text, TouchableOpacity } from "react-native";
+
+
+export default function HomeScreen(){
+
+}
---
+++ TecPizzaApp/source/navigation/StackNavigator.js
@@ -0,0 +1,13 @@
+import react from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../components/HomeScreen";
+
+const Stack = createNativeStackNavigator()
+
+export default function StackNavigator(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name = "Home" component={HomeScreen}/>
+        </Stack.Navigator>
+    )
+}
